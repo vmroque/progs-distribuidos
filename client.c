f4795da99e94944127b4ad96c019ab92c22f4e42
Victor Roque <vmr1@icomp.ufam.edu.br>
2019-04-06 10:57:44 -0400

    Comunicacao cliente-servidor com conexao continua

--- client.c
+++ client.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -8,19 +9,23 @@
 
 int main(int argc, char **argv)
 {
+	int sock;
 	struct sockaddr_in serv_addr;
-	char msg[100];
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	char msg[20];
 
-	printf("Digite sua mensagem para %s: ", argv[1]);
-	scanf("%s", msg);
+	for (;;) {
+		sock = socket(AF_INET, SOCK_STREAM, 0);
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
-	inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_port = htons(PORT);
+		inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
 
-	connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	send(sock, msg, strlen(msg), 0);
+		connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+		scanf("%s", msg);
+		send(sock, msg, sizeof(msg), 0);
+
+		close(sock);
+	}
 
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
 #define PORT 8080
+
+double juros(double q0, double qf, int t)
+{
+	return pow(q0/qf, 1.0/t) - 1.0;
+}
 
 int main(int argc, char **argv)
 {
 	int server_fd, new_socket;
 	struct sockaddr_in address;
 	int addrlen = sizeof(address);
-	char buffer[1024];
+	int len;
+	char buffer[256];
 
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -21,9 +29,17 @@ int main(int argc, char **argv)
 
 	bind(server_fd, (struct sockaddr *)&address, sizeof(address));
 	listen(server_fd, 3);
-	new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-	read(new_socket, buffer, 1024);
-	printf("%s\n", buffer);
+
+	for (;;) {
+		new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+		len = read(new_socket, buffer, 256);
+		buffer[len] = 0;
+		printf("%s\n", buffer);
+
+		close(new_socket);
+	}
+
+	close(server_fd);
 
 	return 0;
 }
