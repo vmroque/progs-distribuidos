0374e70c03e654353893eceb79f171ed51fcc1f9
Victor Roque <vmr1@icomp.ufam.edu.br>
2019-04-08 14:08:17 -0400

    Tratamento de excecoes no lado cliente

--- client.c
+++ client.c
@@ -15,21 +15,33 @@
 #define PORT 8080
 #define BUFFER_SIZE 256
 #define N_ENTRIES  3
+#define ERROR(m) do {				\
+			printf(m);		\
+			exit(EXIT_FAILURE);	\
+		} while(0)
 
 int main(int argc, char **argv)
 {
-	int sock, i;
+	int sock, status, i;
 	struct sockaddr_in serv_addr;
 	char input[20];
 	char buffer[BUFFER_SIZE];
 
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
-	inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
+	status = inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
+
+	if (status <= 0)
+		ERROR("Invalid address\n");
 
 	for (;;) {
 		sock = socket(AF_INET, SOCK_STREAM, 0);
-		connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+		if (sock < 0)
+			ERROR("Socket failed\n");
+
+		status = connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+		if (status < 0)
+			ERROR("Connection failed\n");
 
 		for (i = 0, buffer[0] = 0; i < N_ENTRIES; i++) {
 			scanf("%s", input);
@@ -37,10 +49,15 @@ int main(int argc, char **argv)
 			strcat(buffer, " ");
 		}
 
-		send(sock, buffer, BUFFER_SIZE, 0);
-		read(sock, buffer, BUFFER_SIZE);
-		printf("juros = %s\n", buffer);
+		status = send(sock, buffer, BUFFER_SIZE, 0);
+		if (status < 0)
+			ERROR("Sending failed\n");
 
+		status = read(sock, buffer, BUFFER_SIZE);
+		if (status < 0)
+			ERROR("Reading failure\n");
+
+		printf("juros = %s\n", buffer);
 		close(sock);
 	}
 
