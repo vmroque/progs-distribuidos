785c4ca12cb68f01f7b1b68b08a790166630e54d
Victor Roque <vmr1@icomp.ufam.edu.br>
2019-04-09 15:37:04 -0400

    GUI do lado cliente melhorada, mas ainda não funcional

--- java-rmi/ClientGUI.java
+++ java-rmi/ClientGUI.java
@@ -6,12 +6,17 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class ClientGUI extends JFrame {
     InterfaceRemota ir;
     JPanel painel;
+    JPanel painelAddInterno;
+    JLabel lbMoeda;
+    JLabel lbCotacao;
     JTextField txtAddMoeda;
-    JTextField txtRemoverMoeda;
+    JTextField txtAddCotacao;
+    JComboBox cbRemoverMoeda;
     JComboBox cbEditarMoeda;
     JButton btnAdicionar;
     JButton btnRemover;
@@ -20,22 +25,32 @@ public class ClientGUI extends JFrame {
     public ClientGUI() {
         super("MCotações (Cliente)");
 
-        txtAddMoeda = new JTextField("Adicionar Moeda");
-        txtRemoverMoeda = new JTextField("Remover Moeda");
+        lbMoeda = new JLabel("Moeda:");
+        lbCotacao = new JLabel("Cotação:");
+        txtAddMoeda = new JTextField("");
+        txtAddCotacao = new JTextField("");
         btnAdicionar = new JButton("Adicionar");
         btnRemover = new JButton("Remover");
         btnEditar = new JButton("Editar");
+        cbRemoverMoeda = new JComboBox();
         cbEditarMoeda = new JComboBox();
     }
 
     public void mostrarJanela() {
         painel = new JPanel();
+        painelAddInterno = new JPanel();
         painel.setLayout(new GridLayout(3,2));
+        painelAddInterno.setLayout(new GridLayout(1,4));
 
-        painel.add(txtAddMoeda);
+        painelAddInterno.add(lbMoeda);
+        painelAddInterno.add(txtAddMoeda);
+        painelAddInterno.add(lbCotacao);
+        painelAddInterno.add(txtAddCotacao);
+
+        painel.add(painelAddInterno);
         painel.add(btnAdicionar);
 
-        painel.add(txtRemoverMoeda);
+        painel.add(cbRemoverMoeda);
         painel.add(btnRemover);
 
         painel.add(cbEditarMoeda);
